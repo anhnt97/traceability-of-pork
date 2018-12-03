495b728240bbbac5a476a403146559986216473c
haohoang97 <haohoanggt97@gmail.com>
2018-12-04 01:13:34 +0700

    abd

--- resources/views/layouts/breed-management.blade.php
+++ resources/views/layouts/breed-management.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Giám sát quy trình</h1>
+                <h1 class="m-0 text-dark">Giám sát chăn nuôi</h1>
             </div><!-- /.col -->
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
