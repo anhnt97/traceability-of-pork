f0ea5de73eee6ee7fbed004ed8fdfa5a85c4a894
haohoang97 <haohoanggt97@gmail.com>
2018-12-04 01:10:13 +0700

    layout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('layouts.master');
+        return view('layouts.dashboard');
     }
     public function breed(){
         $batches = DB::table('batches')->paginate(10);
