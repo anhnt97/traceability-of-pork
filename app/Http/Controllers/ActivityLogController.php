578bb78bd74235a150a5a58693f73e1e2456dfbe
anhnt97 <tuananh05031997@gmail.com>
2018-12-09 17:30:02 +0700

    update controller by anhnt

---
+++ app/Http/Controllers/ActivityLogController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Activitylog\Models\Activity;
+
+class ActivityLogController extends Controller
+{
+    public function index(){
+        $activityLogAll = Activity::all();
+        return view('activity-log',['activityLogAll' => $activityLogAll]);
+    }
+    public function cleanLog(){
+        Activity::cleanLog();
+    }
+    public function searchLogByName($logName){
+        $log = Activity::where('log_name' , $logName)->get();
+        return view('activity-log',['activityLog' => $log]);
+    }
+}
